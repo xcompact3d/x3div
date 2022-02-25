xcompact3d/x3div

commit 184c22029517f33c089c99d086a451c7d22f30cc

Remove dependency of x3d_operator_*_data on module_param.f90

src/x3d_tools.f90
@@ -171,9 +171,9 @@ subroutine init_xcompact3d(ndt_max)
   call decomp_info_init(nxm, nym, nz, ph3)
 
   call var_init()
-  call x3d_operator_x_data_init()
-  call x3d_operator_y_data_init()
-  call x3d_operator_z_data_init()
+  call x3d_operator_x_data_init(nx, nzm)
+  call x3d_operator_y_data_init(ny, nym)
+  call x3d_operator_z_data_init(nz, nzm)
   call x3d_operator_1d_init()
   call x3d_derive_init()
 
@@ -195,6 +195,7 @@ subroutine finalise_xcompact3d(flag)
   use x3d_operator_1d, only : x3d_operator_1d_finalize
   use x3d_derive, only : x3d_derive_finalize
   use var, only : var_finalize
+  use variables, only : nz
 
   implicit none
 
@@ -211,7 +212,7 @@ subroutine finalise_xcompact3d(flag)
   call x3d_operator_1d_finalize()
   call x3d_operator_x_data_finalize()
   call x3d_operator_y_data_finalize()
-  call x3d_operator_z_data_finalize()
+  call x3d_operator_z_data_finalize(nz)
   call var_finalize()
 
   call decomp_2d_finalize()

src/x3d_operator_x_data.f90
@@ -5,7 +5,6 @@
 module x3d_operator_x_data
 
    use decomp_2d, only: mytype
-   use param, only: zero
 
    implicit none
 
@@ -55,14 +54,14 @@ contains
    !
    ! Allocate memory
    !
-   subroutine x3d_operator_x_data_init()
-
-      use variables, only: nx, nxm
+   subroutine x3d_operator_x_data_init(nx, nxm)
 
       implicit none
 
+      integer, intent(in) :: nx, nxm
+
       allocate (ffx(nx))
-      ffx = zero
+      ffx = 0._mytype
       allocate (sfx, source=ffx)
       allocate (fsx, source=ffx)
       allocate (fwx, source=ffx)
@@ -91,7 +90,7 @@ contains
       allocate (swxpS, source=ffx)
 
       allocate (cfx6(nxm))
-      cfx6 = zero
+      cfx6 = 0._mytype
       allocate (ccx6, source=cfx6)
       allocate (cbx6, source=cfx6)
       allocate (cfxp6, source=cfx6)

src/x3d_operator_y_data.f90
@@ -5,7 +5,6 @@
 module x3d_operator_y_data
 
    use decomp_2d, only: mytype
-   use param, only: zero
 
    implicit none
 
@@ -55,14 +54,14 @@ contains
    !
    ! Allocate memory and prepare arrays
    !
-   subroutine x3d_operator_y_data_init()
-
-      use variables, only: ny, nym
+   subroutine x3d_operator_y_data_init(ny, nym)
 
       implicit none
 
+      integer, intent(in) :: ny, nym
+
       allocate (ffy(ny))
-      ffy = zero
+      ffy = 0._mytype
       allocate (sfy, source=ffy)
       allocate (fsy, source=ffy)
       allocate (fwy, source=ffy)
@@ -91,7 +90,7 @@ contains
       allocate (swypS, source=ffy)
 
       allocate (cfy6(nym))
-      cfy6 = zero
+      cfy6 = 0._mytype
       allocate (ccy6, source=cfy6)
       allocate (cby6, source=cfy6)
       allocate (cfyp6, source=cfy6)

src/x3d_operator_z_data.f90
@@ -5,7 +5,6 @@
 module x3d_operator_z_data
 
    use decomp_2d, only: mytype
-   use param, only: zero
 
    implicit none
 
@@ -55,16 +54,16 @@ contains
    !
    ! Allocate memory and prepare arrays
    !
-   subroutine x3d_operator_z_data_init()
-
-      use variables, only: nz, nzm
+   subroutine x3d_operator_z_data_init(nz, nzm)
 
       implicit none
+
+      integer, intent(in) :: nz, nzm
 
       if (nz==1) return
 
       allocate (ffz(nz))
-      ffz = zero
+      ffz = 0._mytype
       allocate (sfz, source=ffz)
       allocate (fsz, source=ffz)
       allocate (fwz, source=ffz)
@@ -93,7 +92,7 @@ contains
       allocate (swzpS, source=ffz)
 
       allocate (cfz6(nzm))
-      cfz6 = zero
+      cfz6 = 0._mytype
       allocate (ccz6, source=cfz6)
       allocate (cbz6, source=cfz6)
       allocate (cfzp6, source=cfz6)
@@ -134,11 +133,11 @@ contains
    !
    ! Free memory
    !
-   subroutine x3d_operator_z_data_finalize()
-
-      use variables, only : nz
+   subroutine x3d_operator_z_data_finalize(nz)
 
       implicit none
+
+      integer, intent(in) :: nz
 
       if (nz==1) return
 

commit 8c8ee362662bfc55fdf39293a343541a4fd2abeb

Fix typo when calling x3d_operator_x_data_init. Bug introduced in 184c22029517f33c089c99d086a451c7d22f30cc

src/x3d_tools.f90
@@ -171,7 +171,7 @@ subroutine init_xcompact3d(ndt_max)
   call decomp_info_init(nxm, nym, nz, ph3)
 
   call var_init()
-  call x3d_operator_x_data_init(nx, nzm)
+  call x3d_operator_x_data_init(nx, nxm)
   call x3d_operator_y_data_init(ny, nym)
   call x3d_operator_z_data_init(nz, nzm)
   call x3d_operator_1d_init()
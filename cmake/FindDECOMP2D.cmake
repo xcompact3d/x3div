ebb4cd909cd73db5da1947ac67c234cca4ea82c6
Stefano Rolfo <stefano.rolfo@stfc.ac.uk>
2023-04-22 10:57:29 +0100

    Update search/install of 2decomp-fft using CMake find_package

--- cmake/FindDECOMP2D.cmake
+++ cmake/FindDECOMP2D.cmake
@@ -1,8 +1,9 @@
 # - Find the 2decomp-fft library
-
-if( NOT D2D_ROOT AND DEFINED ENV{D2D_DIR} )
-  set( D2D_ROOT $ENV{D2D_DIR} )
-else()
+find_package(decomp2d
+             PATHS ${CMAKE_SOURCE_DIR}/decomp2d/build)
+if (decomp2d_FOUND)
+  message(STATUS "2decomp-fft FOUND")
+else(decomp2d_FOUND)
   message(STATUS "2decomp-fft PATH not available we'll try to download and install")
   configure_file(${CMAKE_SOURCE_DIR}/cmake/decomp2d/downloadBuild2decomp.cmake.in decomp2d-build/CMakeLists.txt)
   #message("Second CMAKE_GENERATOR ${CMAKE_GENERATOR}") 
@@ -20,20 +21,10 @@ else()
   if(result)
       message(FATAL_ERROR "Build step for 2decomp-fft failed: ${result}")
   endif()
-  set(D2D_ROOT ${CMAKE_CURRENT_BINARY_DIR}/decomp2d-build/downloadBuild2decomp-prefix/src/downloadBuild2decomp-build/opt)
-endif()
-message(STATUS "D2D_ROOT ${D2D_ROOT}")
+  set(D2D_ROOT ${CMAKE_CURRENT_BINARY_DIR}/decomp2d-build/downloadBuild2decomp-prefix/src/downloadBuild2decomp-build)
+  find_package(decomp2d REQUIRED
+	  PATHS ${D2D_ROOT})
+endif(decomp2d_FOUND)
 
-# find libs
-find_library(
-    DECOMP2D_FFT_LIB
-    NAMES "decomp2d" libdecomp2d
-    PATHS ${D2D_ROOT}
-    PATH_SUFFIXES "lib" "lib64"
-    NO_DEFAULT_PATH
-    REQUIRED
-    )
-set(2decomp_INCLUDE_DIR "${D2D_ROOT}/include")
-message(STATUS "D2D found at: ${D2D_ROOT}")
 
 
